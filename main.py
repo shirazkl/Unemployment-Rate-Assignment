838ac0493eb48975244372cbf838e3257f737a78
shirazkl <shirazkl18@gmail.com>
2022-08-06 14:12:16 +0300

    Finish the assignment without testing

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import os
 from datetime import datetime
-
+from typing import List
 from utils import get_json_data_from_bls, save_json, create_parquet_table
 from logger_file import logger
 from configs import series_id, start_year, end_year, raw_data_dir
 
 
-def main(series_id=series_id, start_year=start_year, end_year=end_year, raw_data_dir=raw_data_dir):
+def main(series_id: List[str] = series_id,
+         start_year: int = start_year,
+         end_year: int = end_year,
+         raw_data_dir: str = raw_data_dir):
+    """
+    Download and store the public unemployment rate data of several local areas in the U.S.
+    The data stored in two layers, the raw data - json files of the data retrieved using the BLS API,
+    the mirror data - parquet table with the columns : 'seriesID', 'year', 'period', 'periodName', 'value', 'footnotes',
+    and with partition by 'seriesID' and 'year'
+    :param series_id: A list of series id that presents the local areas for which you want to receive the desired data
+    :param start_year: The year from which the data is desired
+    :param end_year: The year up to which the data is desired
+    :param raw_data_dir: The directory path to store the raw data in.
+    """
+
     try:
         json_data = get_json_data_from_bls(series_id, start_year, end_year)
     except Exception as error:
--- utils.py
+++ utils.py
@@ -6,13 +6,14 @@ import os
 from logger_file import logger
 
 
-def get_json_data_from_bls(series_id: list, start_year: int, end_year: int):
+def get_json_data_from_bls(series_id: list, start_year: int, end_year: int) -> dict:
     """
-
-    :param series_id:
-    :param start_year:
-    :param end_year:
-    :return:
+    Using BlS API to get json data of unemployment rates of the given series (that represent local areas)
+     between the given years
+    :param series_id: A list of series id that presents the local areas for which you want to receive the desired data
+    :param start_year: The year from which the data is desired
+    :param end_year: The year up to which the data is desired
+    :return: json with the desired data.
     """
 
     # Set request variables.
@@ -24,6 +25,7 @@ def get_json_data_from_bls(series_id: list, start_year: int, end_year: int):
                 f"between the years {start_year} and {end_year}.")
     response = requests.post(url, json=payload, headers=headers)
     json_data = response.json()
+    logger.info(json_data['status'])
 
     if len(json_data['message']) > 0:
         logger.warning(json_data['message'])
@@ -31,15 +33,33 @@ def get_json_data_from_bls(series_id: list, start_year: int, end_year: int):
     return json_data
 
 
-def save_json(file_path: str, json_data: json):
+def save_json(file_path: str, json_data: dict):
+    """
+    Save json data into json file in the given path
+    :param file_path: The file path to save the data to
+    :param json_data: Json with the data to saved
+    """
+
+    # If the directory to which the save is made doesn't exist, then create it.
     dir_name = os.path.dirname(file_path)
     os.makedirs(dir_name, exist_ok=True)
 
+    # Save the data to a json file
     with open(file_path, 'w') as file:
         json.dump(json_data, file)
 
 
-def create_parquet_table(json_data, raw_file, received_date, parquet_dir="./data"):
+def create_parquet_table(json_data: dict, raw_file: str, received_date: str, parquet_dir: str = "./data"):
+    """
+    Create parquet table from the json_data with the columns : 'seriesID', 'year', 'period', 'periodName', 'value',
+     'footnotes', and with partition by 'seriesID' and 'year'
+    :param json_data: The raw json downloaded using the BLS API ,of the unemployment rates of some local areas
+    between some years
+    :param raw_file: The path of the file that contains the raw json_data
+    :param received_date: The date in which the data was downloaded from BLS
+    :param parquet_dir: The directory path to store parquet table in.
+    """
+
     # Create an empty DataFrame.
     final_df = pd.DataFrame([])
 
